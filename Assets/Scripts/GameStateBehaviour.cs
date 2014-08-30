3f7a8a52ffbd3f4316e7b95d6cc18abad4001d82
Daniel Savoia <danielsavoia@gmail.com>
2014-08-30 05:42:11 -0300

    Transicao de cenarios v1

---
+++ Assets/Scripts/GameStateBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateBehaviour : MonoBehaviour {	
+	
+	//1 for Happy, -1 for Sad
+	public static int gameState;	
+	
+	void Start () {
+		gameState = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C)){
+			gameState *= -1; 
+			Debug.Log("gameState: " + gameState);
+		}
+	}	
+	
+}
---
+++ Assets/Scripts/ObjectsBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectsBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	private int previousState;
+	private int actualState;
+	private Animator animator;
+	
+	void Start() {
+		actualState = 1;
+		previousState = 1;
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		actualState = GameStateBehaviour.gameState;
+		
+		if(actualState != previousState){
+			Debug.Log("prevStates: " + previousState + " actualState: " + actualState);
+			ChangeState();
+			previousState = actualState;
+		}
+		
+	}
+	
+	void ChangeState(){
+		Debug.Log(gameObject.name + " Changing State " + actualState);
+		animator.SetTrigger("ChangeState");
+	}	
+}
