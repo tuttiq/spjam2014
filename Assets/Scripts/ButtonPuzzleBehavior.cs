ae682efaf52ad47c4339916d865ab80af0fb8514
Daniel Savoia <danielsavoia@gmail.com>
2014-08-31 17:23:39 -0300

    Vai puuuuuzzle do botao

---
+++ Assets/Scripts/ButtonPuzzleBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPuzzleBehavior : MonoBehaviour {
+
+	ButtonPuzzleManagerBehavior puzzleManager;
+	bool isPressed;
+	
+	// Use this for initialization
+	void Start () {
+		puzzleManager = GameObject.FindGameObjectWithTag("ButtonPuzzleManager").GetComponentInParent<ButtonPuzzleManagerBehavior>();
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {	
+		if (collision.gameObject.tag == "Player" && !isPressed) {
+			isPressed = true;
+			puzzleManager.VerifyPuzzle(gameObject.tag);
+		}
+	}
+}
