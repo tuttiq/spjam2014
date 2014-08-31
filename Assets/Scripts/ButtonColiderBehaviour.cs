0bafe3753657435d868246b33ab666a7657a8d41
Davi Nakano <daviycnakano@gmail.com>
2014-08-31 13:28:49 -0300

    New Button

---
+++ Assets/Scripts/ButtonColiderBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonColiderBehaviour : ObjectsBehaviour {
+
+	public float offsetCollider;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player") 
+		{
+			if (currentState() == GameStateBehaviour.STATES.Sad)
+			{
+				BoxCollider2D box = gameObject.GetComponent<BoxCollider2D>();
+				box.size = new Vector2(box.size.x, box.size.y - offsetCollider);
+			}
+			else
+			{
+				BoxCollider2D box = gameObject.GetComponent<BoxCollider2D>();
+				box.size = new Vector2(box.size.x, box.size.y + offsetCollider);
+			}
+		}
+
+	}
+}
