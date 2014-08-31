a06f55c8aa788a025081d91772300152f4666e5d
Davi Nakano <daviycnakano@gmail.com>
2014-08-31 18:15:49 -0300

    Main menu ok

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	void OnGUI() {
-		if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/3, Screen.width/5, Screen.height/10), "New Game")) {
+		if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/2, Screen.width/5, Screen.height/10), "New Game")) {
 			Application.LoadLevel("Tutorial");
 		}
 
-		if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/2, Screen.width/5, Screen.height/10), "About")) {
+		if(GUI.Button(new Rect(Screen.width/2.5f, 400, Screen.width/5, Screen.height/10), "About")) {
 			Application.LoadLevel("About");
 		}
 	}
