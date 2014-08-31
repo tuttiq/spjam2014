d22aab3c886a0ad59d9de30127255f92c703331c
carlos <carlos.h.rodrigues.a@gmail.com>
2014-08-31 17:53:50 -0300

    Fix some problems on Tutorial

---
+++ Assets/EndStage1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndStage1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		
+		Application.LoadLevel ("SegundaFase");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
