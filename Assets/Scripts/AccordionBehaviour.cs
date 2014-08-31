f2ae04d5928be6eb2dd3231358adfd1023664fcf
Daniel Teixeira <daniel.t.dt@gmail.com>
2014-08-30 21:06:43 -0300

    wip 2

--- Assets/Scripts/AccordionBehaviour.cs
+++ Assets/Scripts/AccordionBehaviour.cs
@@ -2,15 +2,34 @@
 using System.Collections;
 
 public class AccordionBehaviour : ObjectsBehaviour {
+	Transform originalParent;
+	
 	protected override void Start() {
 		base.Start ();
 	}
 	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			originalParent = coll.transform.parent;
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			coll.transform.parent = transform;
+	}
+	
+	void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			coll.transform.parent = originalParent;
+	}
+	
 	public void compress() {
+		animator.ResetTrigger("Expand");
 		animator.SetTrigger("Compress");
 	}
 	
 	public void expand() {
+		animator.ResetTrigger("Compress");
 		animator.SetTrigger("Expand");
 	}
 }
