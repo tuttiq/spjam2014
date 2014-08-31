73f498903eebe5725d39aa81522b2b06d36e94f9
Tuane Quintella <q.tuane@gmail.com>
2014-08-31 18:34:06 -0300

    avout

--- Assets/Scripts/About.cs
+++ Assets/Scripts/About.cs
@@ -4,9 +4,8 @@ using System.Collections;
 public class About : MonoBehaviour {
 
 	void OnGUI() {
-		GUI.Box(new Rect(Screen.width/2.5f, Screen.height/2, Screen.width/2, Screen.height/3), "FEIJAO DO VERAO D:");
 		
-		if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/2, Screen.width/5, Screen.height/10), "Back")) {
+		if(GUI.Button(new Rect(Screen.width/3.3f, Screen.height/1.4f, Screen.width/8, Screen.height/10), "Back")) {
 			Application.LoadLevel("MainMenu");
 		}
 	}
