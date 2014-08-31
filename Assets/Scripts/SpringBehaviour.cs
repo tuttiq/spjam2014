11e4ea2750dad47c01913ac8776f8e8cb7e38ef1
Daniel Savoia <danielsavoia@gmail.com>
2014-08-31 02:32:29 -0300

    Ajustando Sanfona

---
+++ Assets/Scripts/SpringBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringBehaviour : MonoBehaviour {
+
+	public float speed;	
+	public GameObject spring;
+	public bool compress;
+	public float yScaleLimit;
+	public float yOffset;
+	// Use this for initialization
+	void Start () {
+		compress = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {				
+		if(compress && spring.transform.localScale.y > yScaleLimit){			
+			spring.transform.localScale = new Vector3(spring.transform.localScale.x,
+												 spring.transform.localScale.y - speed,
+												 spring.transform.localScale.z);
+			spring.transform.position = new Vector3(spring.transform.position.x,
+													spring.transform.position.y - speed*2.5f,
+													spring.transform.position.z);			
+																					
+			transform.position = new Vector3(transform.position.x,
+											spring.transform.position.y+yOffset,
+											transform.position.z);		
+		} else if (!compress && spring.transform.localScale.y < 1){			
+			spring.transform.localScale = new Vector3(spring.transform.localScale.x,
+			                                          spring.transform.localScale.y + speed,
+			                                          spring.transform.localScale.z);
+			spring.transform.position = new Vector3(spring.transform.position.x,
+			                                        spring.transform.position.y + speed*2.5f,
+			                                        spring.transform.position.z);		
+			
+			transform.position = new Vector3(transform.position.x,
+			                                 spring.transform.position.y+yOffset,
+			                                 transform.position.z);	
+		 }
+	}
+}
